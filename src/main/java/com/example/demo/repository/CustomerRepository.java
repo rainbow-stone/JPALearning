dbcc5e0d40d2852026d987301c75a0b0d37bdaf2
lin.jie <lin.jie>
2022-08-02 22:08:59 +0800

    Initial 1

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -17,23 +17,29 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>,
   /**      JPA提供方法解析完成简单查询                          **/
   List<Customer> findAllByCustomerId(Long customerId);
 
+  /**      JPQL查询                                         **/
   @Query("select c from Customer c where c.name like concat('%', ?1, '%') or c.name like concat('%', ?2)")
   List<Customer> findAllByNameContainingOrNameEndingWith(String name);
 
   @Query(value = "SELECT c FROM Customer c WHERE c.name = ?1 and c.idType = ?2 and c.idCode = ?3",
         countQuery = "SELECT c FROM Customer c WHERE c.name = ?1 and c.idType = ?2 and c.idCode = ?3")
-  List<Customer> findAllByMutipleConidtionSample1(String name, Integer idType, String idCode);
+  List<Customer> findAllByMultipleConditionSample1(String name, Integer idType, String idCode);
 
   @Query(value = "SELECT c FROM Customer c WHERE c.name = :name and c.idType = :idType and c.idCode = :idCode",
     countQuery = "SELECT c FROM Customer c WHERE c.name = :name and c.idType = :idType and c.idCode = :idCode")
-  List<Customer> findAllByMutipleConidtionSample2(String name, Integer idType, String idCode);
+  List<Customer> findAllByMultipleConditionSample2(String name, Integer idType, String idCode);
 
-  @Query(value = "SELECT c FROM Customer c WHERE c.name = :name and c.idType = :idType and c.idCode = :idCode",
+  @Query(value = "SELECT c FROM Customer c WHERE c.name = #{#name} and c.idType = #{#idType} and c.idCode = #{#idCode}",
     countQuery = "SELECT c FROM Customer c WHERE c.name = :name and c.idType = :idType and c.idCode = :idCode")
-  List<Customer> findAllByMutipleConidtionSample3(String name, Integer idType, String idCode);
+  List<Customer> findAllByMultipleConditionSample3(String name, Integer idType, String idCode);
 
   @Query(value = "SELECT c FROM Customer c WHERE c.name = #{#customer.name} and c.idType = #{#customer.idType} and c.idCode = #{#custom.idCode}",
     countQuery = "SELECT c FROM Customer c WHERE c.name = #{#customer.name} and c.idType = #{#customer.idType} and c.idCode = #{#custom.idCode}")
-  List<Customer> findAllByMutipleConidtionSample4(Customer customer);
+  List<Customer> findAllByMultipleConditionSample4(Customer customer);
+
+  /**      Native SQL查询                                   **/
+  @Query(value = "SELECT c FROM t_customer c WHERE c.name = ?1 and c.idType = ?2 and c.idCode = ?3",
+          countQuery = "SELECT c FROM t_customer c WHERE c.name = ?1 and c.idType = ?2 and c.idCode = ?3")
+  List<Customer> findAllByMultipleConditionSample5(String name, Integer idType, String idCode);
 
 }
--- src/test/java/com/example/demo/PodamService.java
+++ src/test/java/com/example/demo/PodamService.java
@@ -16,57 +16,61 @@ import uk.co.jemos.podam.typeManufacturers.TypeManufacturer;
 @TestComponent
 public class PodamService {
 
-    public PodamFactory createFactory() {
-        PodamFactory podamFactory = new PodamFactoryImpl();
-        final IntTypeManufacturerImpl intManufacturer = new IntTypeManufacturerImpl() {
-            @Override
-            public Integer getInteger(AttributeMetadata attributeMetadata) {
-                if (attributeMetadata.getPojoClass() == Timestamp.class) {
-                    return PodamUtils.getIntegerInRange(0, 999999999);
-                } else {
-                    return super.getInteger(attributeMetadata);
-                }
+  public PodamFactory createFactory() {
+    PodamFactory podamFactory = new PodamFactoryImpl();
+    final IntTypeManufacturerImpl intManufacturer =
+        new IntTypeManufacturerImpl() {
+          @Override
+          public Integer getInteger(AttributeMetadata attributeMetadata) {
+            if (attributeMetadata.getPojoClass() == Timestamp.class) {
+              return PodamUtils.getIntegerInRange(0, 999999999);
+            } else {
+              return super.getInteger(attributeMetadata);
             }
+          }
         };
 
-        final LongTypeManufacturerImpl longManufacturer = new LongTypeManufacturerImpl() {
-            @Override
-            public Long getLong(AttributeMetadata attributeMetadata) {
-                if (attributeMetadata.getPojoClass() == Timestamp.class) {
-                    return PodamUtils.getLongInRange(0, 999999999);
-                } else {
-                    return super.getLong(attributeMetadata);
-                }
+    final LongTypeManufacturerImpl longManufacturer =
+        new LongTypeManufacturerImpl() {
+          @Override
+          public Long getLong(AttributeMetadata attributeMetadata) {
+            if (attributeMetadata.getPojoClass() == Timestamp.class) {
+              return PodamUtils.getLongInRange(0, 999999999);
+            } else {
+              return super.getLong(attributeMetadata);
             }
+          }
         };
 
-        final StringTypeManufacturerImpl stringManufacturer = new StringTypeManufacturerImpl() {
-            @Override
-            public String getStringValue(AttributeMetadata attributeMetadata) {
-                return String.valueOf(PodamUtils.getIntegerInRange(1, 9));
-            }
+    final StringTypeManufacturerImpl stringManufacturer =
+        new StringTypeManufacturerImpl() {
+          @Override
+          public String getStringValue(AttributeMetadata attributeMetadata) {
+            return String.valueOf(PodamUtils.getIntegerInRange(1, 9));
+          }
 
-            @Override
-            public String getStringOfLength(int length, AttributeMetadata attributeMetadata) {
-                return String.valueOf(PodamUtils.getIntegerInRange(1, 9));
-            }
-
+          @Override
+          public String getStringOfLength(int length, AttributeMetadata attributeMetadata) {
+            return String.valueOf(PodamUtils.getIntegerInRange(1, 9));
+          }
         };
 
-        final TypeManufacturer<BigDecimal> doubleTypeManufacturer = (strategy, attributeMetadata, genericTypesArgumentsMap) ->
-                BigDecimal.valueOf(PodamUtils.getDoubleInRange(0, 10000000D)).setScale(2,
-                  RoundingMode.HALF_DOWN);
-        podamFactory.getStrategy().addOrReplaceTypeManufacturer(int.class, intManufacturer);
-        podamFactory.getStrategy().addOrReplaceTypeManufacturer(Integer.class, intManufacturer);
-        podamFactory.getStrategy().addOrReplaceTypeManufacturer(Long.class, longManufacturer);
-        podamFactory.getStrategy().addOrReplaceTypeManufacturer(BigDecimal.class, doubleTypeManufacturer);
-        podamFactory.getStrategy().addOrReplaceTypeManufacturer(String.class, stringManufacturer);
+    final TypeManufacturer<BigDecimal> doubleTypeManufacturer =
+        (strategy, attributeMetadata, genericTypesArgumentsMap) ->
+            BigDecimal.valueOf(PodamUtils.getDoubleInRange(0, 10000000D))
+                .setScale(2, RoundingMode.HALF_DOWN);
+    podamFactory.getStrategy().addOrReplaceTypeManufacturer(int.class, intManufacturer);
+    podamFactory.getStrategy().addOrReplaceTypeManufacturer(Integer.class, intManufacturer);
+    podamFactory.getStrategy().addOrReplaceTypeManufacturer(Long.class, longManufacturer);
+    podamFactory
+        .getStrategy()
+        .addOrReplaceTypeManufacturer(BigDecimal.class, doubleTypeManufacturer);
+    podamFactory.getStrategy().addOrReplaceTypeManufacturer(String.class, stringManufacturer);
 
+    DefaultClassInfoStrategy classInfoStrategy = DefaultClassInfoStrategy.getInstance();
 
-        DefaultClassInfoStrategy classInfoStrategy = DefaultClassInfoStrategy.getInstance();
-
-        classInfoStrategy.addExcludedField(Customer.class, "needAuthCheck");
-        podamFactory.setClassStrategy(classInfoStrategy);
-        return podamFactory;
-    }
+    classInfoStrategy.addExcludedField(Customer.class, "needAuthCheck");
+    podamFactory.setClassStrategy(classInfoStrategy);
+    return podamFactory;
+  }
 }
